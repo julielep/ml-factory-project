042c01f0785f418a7f3906fa5d6bff92dab98ae5
Julie Lep <julieleproux01@gmail.com>
2026-03-16 14:27:20 +0100

    fonction

---
+++ src/api/main.py
@@ -0,0 +1,21 @@
+import os
+
+import boto3
+
+os.environ['MLFLOW_S3_ENDPOINT_URL'] = "http://localhost:9000"
+os.environ['AWS_ACCESS_KEY_ID'] = "minioadmin"
+os.environ['AWS_SECRET_ACCESS_KEY'] = "minioadmin"
+
+def prepare_minio():
+    """Vérifie si le bucket 'mlflow' existe, sinon le crée"""
+
+
+    s3 = boto3.client('s3', endpoint_url=os.environ['MLFLOW_S3_ENDPOINT_URL'])
+
+
+    buckets = [b['Name'] for b in s3.list_buckets()['Buckets']]
+
+
+    if 'mlflow' not in buckets:
+        s3.create_bucket(Bucket='mlflow')
+        print("Bucket 'mlflow' créé avec succès.")
---
+++ src/train/train.py
@@ -0,0 +1,59 @@
+import mlflow
+import mlflow.sklearn
+from mlflow import MlflowClient
+from services.prep_data import prepare_data
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+
+def train_and_register(model, params, X_train, X_test, y_train, y_test):
+    # Configuration MLflow AVANT le run
+    mlflow.set_tracking_uri("http://localhost:5000")
+    mlflow.set_experiment("My_Experiment")
+
+    model_name = "iris_model"
+
+    with mlflow.start_run():
+        # 1. Entraînement
+        model.fit(X_train, y_train)
+        preds = model.predict(X_test)
+
+        # 2. Métriques
+        accuracy = accuracy_score(y_test, preds)
+
+        # 3. Log des paramètres et métriques
+        mlflow.log_params(params)
+        mlflow.log_metric("accuracy", accuracy)
+
+        # 4. Log + enregistrement du modèle dans MLflow Model Registry
+        mlflow.sklearn.log_model(
+            sk_model=model,
+            artifact_path="RandomForest",
+            registered_model_name=model_name
+        )
+
+    print(f"✅ Modèle enregistré dans MLflow Registry : {model_name}")
+
+
+# Chargement des données
+X_train, X_test, y_train, y_test = prepare_data()
+
+# Modèle
+rf_model = RandomForestClassifier()
+
+# Paramètres à logger
+params = rf_model.get_params()
+
+# Entraîner + enregistrer
+train_and_register(rf_model, params, X_train, X_test, y_train, y_test)
+
+# 3. Gestion de l'Alias 'Production' via MlflowClient 
+client = MlflowClient()
+
+# On récupère la toute dernière version créée 
+latest_version = client.get_latest_versions("iris_model", stages=["None"])[0].version
+
+# On lui attribue l'alias 'Production'
+client.set_registered_model_alias("iris_model", "Production", latest_version)
+
+print(f"✅ Alias 'Production' assigné à la version {latest_version} de iris_model")
